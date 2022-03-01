25ca7ec265f47fb4c971b0f49853b8a9c63e9291
Rojas Rafael <rafael.rojas@unibz.it>
2022-03-01 11:06:59 +0100

    wio

--- docker/container-start.bash
+++ docker/container-start.bash
@@ -36,36 +36,15 @@ main(){
         exit 1
     }
 
-    if [[ $1 != "--" ]] && [[ $1=="--network" ]]; then
-        shift
-        if [[ $1 == "smf" ]]; then
 
-        DOCKER_NETWORK_OPTIONS="--net=br0 --env=ROS_MASTER_URI=http://10.10.238.1:11311 --env=ROS_IP=10.10.238.5 --ip 10.10.238.5 "
-
-        if [[ ! $(docker network ls | grep br0 ) ]]; then
-            docker network create --driver=bridge --ip-range=10.10.238.0/24 --subnet=10.10.238.0/24 --aux-address='ip1=10.10.238.4' -o "com.docker.network.bridge.name=br0" br0
-        fi
-    fi
-	    if [[ $1 == "fhi" ]]; then
-
-		    DOCKER_NETWORK_OPTIONS="--net=br0 --env=ROS_MASTER_URI=http://172.16.0.203:11311 --env=ROS_IP=172.16.0.202 --ip 172.16.0.202"
-
-		    if [[ ! $(docker network ls | /bin/grep br0 ) ]]; then
-			    docker network create --driver=bridge --ip-range=172.16.0.0/24 --subnet=172.16.0.0/24 --aux-address='ip1=172.16.0.201' -o "com.docker.network.bridge.name=br0" br0
-		    fi
-	    fi
-    else
-
-        DOCKER_NETWORK_OPTIONS="--env=ROS_MASTER_URI=http://127.0.0.1:11311 --env=ROS_MASTER_IP=127.0.0.1 --env=ROS_IP=127.0.0.1"
-    fi
+    DOCKER_NETWORK_OPTIONS="--env=ROS_MASTER_URI=http://127.0.0.1:11311 --env=ROS_MASTER_IP=127.0.0.1 --env=ROS_IP=127.0.0.1"
 
     docker run -it --rm \
         ${DOCKER_VIDEO_OPTIONS} \
         ${DOCKER_NETWORK_OPTIONS} \
-        --privileged \
         --volume $(pwd)/../:/catkinws/src/ \
         --user $(id -u):$(id -g) \
-        opstop_ros bash
+        rafa606/ros-ik-example bash
 }
 
 main $@
